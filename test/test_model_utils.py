63439ff48ec4bc78ea015e7dcd758c6cd313815c
esavary <elodie.savary@outlook.com>
2024-05-27 17:33:14 +0200

    enh: add test for positive definite kernel

--- test/test_model_utils.py
+++ test/test_model_utils.py
@@ -1,3 +1,26 @@
+
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+#
+# Copyright 2024 The NiPreps Developers <nipreps@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# We support and encourage derived works from this project, please read
+# about our expectations at
+#
+#     https://www.nipreps.org/community/licensing/
+#
 import numpy as np
 import pytest
 
@@ -22,6 +45,10 @@ def test_kernel_call():
 
     # Assert the kernel matrix is as expected
     np.testing.assert_array_almost_equal(K, expected_K, decimal=6)
+
+    # Check if the kernel matrix is positive definite
+    eigenvalues = np.linalg.eigvals(K)
+    assert np.all(eigenvalues > 0), "Kernel matrix is not positive definite"
 
 def test_kernel_diag():
     # Create a SphericalCovarianceKernel instance
